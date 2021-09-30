88f6eabca6f522fdf64aa01a53382cf2efdf41d0
Initial commits <>
2021-09-30 16:55:19 +0800

    Upload scene

--- src/game.ts
+++ src/game.ts
@@ -5,9 +5,4 @@ const baseScene = new Entity();
 engine.addEntity(baseScene);
 
 // Give it a component for the model itself
-// baseScene.addComponent(new GLTFShape("models/scene.glb"));
-
-const sofa = new Entity();
-engine.addEntity(sofa);
-sofa.addComponent(new GLTFShape("models/Wooden_Sofa/substance _file.glb"));
-sofa.addComponent(new Transform({ position: new Vector3(21.18, 0, 12.5) }));
+baseScene.addComponent(new GLTFShape("models/scene.glb"));
